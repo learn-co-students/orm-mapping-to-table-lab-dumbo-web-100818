db993afc6a51e32a5b656a8b31010ef224b33bed
github username <chao.alexander.123@gmail.com>
2018-10-19 10:40:21 -0400

    defined save and create methods for student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,24 +1,38 @@
 class Student
-  attr_reader :name, :grade
+  attr_reader :id, :name, :grade
 
-  def initialize(name, grade)
+  def initialize(id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
   end
 
   def self.create_table
-    DB[:conn].execute(<<-SQL
-        CREATE TABLE students (
+    DB[:conn].execute(
+        "CREATE TABLE students (
           id INTEGER PRIMARY KEY,
           name TEXT,
           grade TEXT
-        )
-      SQL
+        )"
     )
   end
 
   def self.drop_table
-    
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    student = Student.new(name, grade)
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", name, grade)
+    # require 'pry'; binding.pry
+    id = DB[:conn].execute("SELECT last_insert_rowid()")
+    @id = id[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    return student
   end
 
 end
