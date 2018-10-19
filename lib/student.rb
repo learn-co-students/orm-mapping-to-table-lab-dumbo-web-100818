1ca7b526e78e997ccde813d78cb1c6fb99f5ed7f
github username <chao.alexander.123@gmail.com>
2018-10-18 20:44:48 -0400

    defined create table method for student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,24 @@
 class Student
+  attr_reader :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    DB[:conn].execute(<<-SQL
+        CREATE TABLE students (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade TEXT
+        )
+      SQL
+    )
+  end
+
+  def self.drop_table
+    
+  end
+
 end
